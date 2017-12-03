a8a0f6c2cc95bc9574ed48140a2d024b39f3089f
Austen Henry <austen@WIN2.CS.KSU.EDU>
2017-12-02 18:05:21 -0600

    Fixed compile issues, got it to at least get to the point where it sends the get. Need to make get request.

--- src/Client.java
+++ src/Client.java
@@ -11,26 +11,41 @@ import java.util.*;
 
 
 public class Client {
+	
+	private static void errorQuit(String message) {
+		System.out.println(message);
+		System.exit(1);
+	}
 
-	private boolean checkAddress(String raw) {
-		return false;
+	private static String  checkAddress(String raw) {
+		if(raw.length() > 100) {
+			return "";
+		}
+		return raw;
 	}
 	
-	private boolean checkPort(String raw) {
-		return false;
+	private static int checkPort(String raw) {
+		int portNumber = -1;
+		try {
+			portNumber = Integer.parseInt(raw);
+		}catch(Exception e) {
+			System.out.println("Invalid port.");
+			portNumber = -1;
+		}
+		return portNumber;
 	}
 	
 	private static String getUserPage() {
 		String userInput = "";
 		try {
-			System.out.println("What page? \n-> ");
+			System.out.println("What page?");
+			System.out.print("-> ");
 			Scanner scanner = new Scanner(System.in);
 			userInput = scanner.nextLine();	
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			System.exit(1);
-		}
-		
+			userInput = "";
+		}		
 		return userInput;
 			 
 	}
@@ -42,21 +57,45 @@ public class Client {
 		
 		// If there are no arguments or more than two, exit.
 		if( numArgs == 0 || numArgs > 2) {
-			System.exit(0);
+			errorQuit("Please enter either web server name, or a web server name and a port as arguments. ");
 		}
 		
 		
 		if ( numArgs == 1) {
+			
+			hostName = checkAddress(args[0]);
+			System.out.println(hostName);
+			System.out.println(numArgs);
 			// Check if it is an address.
 			// If not exit.
+			if(hostName.equals("")){
+				errorQuit("This was not a valid web server address.");
+			}
+			else {
+				hostName = args[0];
+				portNumber = 80;
+			}
 		}
 		else {
 			// Must be 2 args. Check if it is an address / port.
 			// If not, exit.
+			hostName = checkAddress(args[0]);
+			portNumber = checkPort(args[1]);
+			if(hostName.equals("") || portNumber == -1) {
+				errorQuit("This was not a valid web server address.");
+			}
+			else {
+				hostName = args[0];
+				portNumber = Integer.parseInt(args[1]);
+			}
 		}
 		
 		try {
 			String userInput = getUserPage();
+			
+			if(userInput.equals("")) {
+				errorQuit("Please enter a valid page to get. ");
+			}
 			
 			Socket mainSock = new Socket (hostName, portNumber);
 			
@@ -73,9 +112,12 @@ public class Client {
 				System.out.println("From server: " + in.readLine());
 			}
 			
+			out.close();
+			in.close();
+			stdIn.close();
+			
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			System.exit(1);
+			errorQuit(e.getMessage());
 		}
 		
 
